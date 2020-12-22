93f6ac389444d2880c75c83bd9be2df1f7420433
Laith <40428921+laithalaboodi@users.noreply.github.com>
2020-12-21 21:21:32 -0700

    connect to mysql
    Simple way to connect to your mysql database using php

---
+++ connnect.php
@@ -0,0 +1,9 @@
+<?php
+$db_name = "Databasename";
+$mysql_username = "username of your mysql";
+$mysql_password = "your mysql password";
+$server_name = "EXAMPLE: example.com";
+$conn = mysqli_connect($server_name, $mysql_username, $mysql_password, $db_name);
+
+//echo "connection to database is success: ";
+?>
